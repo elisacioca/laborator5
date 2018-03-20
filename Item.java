22ff358f0c63089c9bfe7db5912ab7fad97ff009
AndraBolohan <andraioana77@gmail.com>
2018-03-20 09:06:44 +0200

    Item class

--- Item.java
+++ Item.java
@@ -1,11 +1,41 @@
+import java.util.Objects;
 import java.util.Vector;
 
 public class Item {
 
-  public int pret;
+  private int pret;
 
-  public int ID;
+  private int ID;
 
-    public Vector  myPachet;
+  public Item(int pret, int ID) {
+    this.pret = pret;
+    this.ID = ID;
+  }
+
+  public int getPret() {
+    return pret;
+  }
+
+  public void setPret(int pret) {
+    this.pret = pret;
+  }
+
+  public int getID() {
+    return ID;
+  }
+
+  public void setID(int ID) {
+    this.ID = ID;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof Item)) return false;
+    Item item = (Item) o;
+    return pret == item.pret &&
+            ID == item.ID;
+  }
+
 
 }
