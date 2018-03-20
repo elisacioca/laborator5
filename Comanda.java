b2556697dea13eaa075d988bae5b4b638b05bc26
banutu98 <serbanstol@yahoo.com>
2018-03-20 09:30:51 +0200

    Refined the Command class
    Added implementations

--- Comanda.java
+++ Comanda.java
@@ -2,24 +2,74 @@ import java.util.Vector;
 
 public class Comanda {
 
-  public String dataPlasare;
+  private String dataPlasare;
 
-  public String dataLivrare;
+  private String dataLivrare;
 
-  public int ID;
+  private int ID;
 
-  public String curier;
+  private String curier;
 
-  public int pretTotal;
+  private int pretTotal;
 
-    /**
-   * 
-   * @element-type Pachet
-   */
-  public Vector  myPachet;
-    public History myHistory;
+  private Vector<Pachet>  myPachet;
+  private History myHistory;
 
   public void newOperation() {
   }
 
+  public String getDataPlasare() {
+    return dataPlasare;
+  }
+
+  public void setDataPlasare(String dataPlasare) {
+    this.dataPlasare = dataPlasare;
+  }
+
+  public String getDataLivrare() {
+    return dataLivrare;
+  }
+
+  public void setDataLivrare(String dataLivrare) {
+    this.dataLivrare = dataLivrare;
+  }
+
+  public int getID() {
+    return ID;
+  }
+
+  public void setID(int ID) {
+    this.ID = ID;
+  }
+
+  public String getCurier() {
+    return curier;
+  }
+
+  public void setCurier(String curier) {
+    this.curier = curier;
+  }
+
+  public int getPretTotal() {
+    return pretTotal;
+  }
+
+  public void setPretTotal(int pretTotal) {
+    this.pretTotal = pretTotal;
+  }
+
+  /**
+ *
+ * @element-type Pachet
+ */
+  public Vector getMyPachet() {
+    return myPachet;
+  }
+
+  public void setMyPachet(Vector myPachet) {
+    this.myPachet = myPachet;
+  }
+  public void add(Pachet pachet){
+    this.myPachet.add(pachet);
+  }
 }
