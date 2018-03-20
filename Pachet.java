a571abf53f76f96ff8c361836a28f4fc5647e6f0
AndraBolohan <andraioana77@gmail.com>
2018-03-20 09:15:53 +0200

    Pachet class

--- Pachet.java
+++ Pachet.java
@@ -2,22 +2,62 @@ import java.util.Vector;
 
 public class Pachet {
 
-  public int status;
+  protected int status;
 
-  public vector<Item> Produse;
+  protected Vector<Item> Produse;
 
-  public int pretPachet;
+  private int pretPachet;
 
-  public int ID;
-
-    public Comanda myComanda;
-    /**
-   * 
-   * @element-type Item
-   */
-  public Vector  myItem;
+  private int ID;
 
   public void getStatus() {
+
+    switch (status) {
+      case 0:
+        System.out.println("preluare depozit");
+      case 1:
+        System.out.println("in drum spre Iasi");
+      case 2:
+        System.out.println("livrare client");
+      case 3:
+        System.out.println("incheiere");
+    }
+
+  }
+
+  public Pachet(int status, Vector<Item> produse, int pretPachet, int ID, Vector myItem) {
+    this.status = status;
+    Produse = produse;
+    this.pretPachet = pretPachet;
+    this.ID = ID;
+  }
+
+  public void setStatus(int status) {
+    this.status = status;
+  }
+
+  public Vector<Item> getProduse() {
+    return Produse;
+  }
+
+  public void setProduse(Vector<Item> produse) {
+    Produse = produse;
+  }
+
+  public int getPretPachet() {
+    return pretPachet;
+  }
+
+  public void setPretPachet(int pretPachet) {
+    this.pretPachet = pretPachet;
+  }
+
+  public int getID() {
+    return ID;
+  }
+
+  public void setID(int ID) {
+    this.ID = ID;
   }
 
 }
